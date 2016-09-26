3ac9d72a33a73a916fae2c299bb31ad8a4372d12
Franco Sarachu <fsarachu@gmail.com>
2016-09-26 17:00:27 -0300

    Define Movie Class and __init__

---
+++ entertainment_center.py
@@ -0,0 +1,3 @@
+import media
+
+toy_story = media.Movie("dummy title", "dummy storyline", "dummy image", "dummy link")
---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, title, storyline, poster_image, youtube_trailer_link):
+        self.title = title
+        self.storyline = storyline
+        self.image = poster_image
+        self.trailer = youtube_trailer_link
