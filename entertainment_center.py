f646741e6fdda1b1f28c7e95270072a908d98a35
Franco Sarachu <fsarachu@gmail.com>
2016-09-26 20:53:00 -0300

    Reformat line

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,7 @@
 import media
 
 the_godfather = media.Movie("The Godfather",
-                            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son. ",
+                            "The aging patriarch of an organized crime dynasty transfers control of his clandestine "
+                            "empire to his reluctant son. ",
                             "http://dramatica.com/resources/onesheets/the-godfather-one-sheet.jpg",
                             "https://www.youtube.com/watch?v=sY1S34973zA")
