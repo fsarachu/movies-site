807b115ef2cf4b6118d0b720bc5c1eb04974524a
Franco Sarachu <fsarachu@gmail.com>
2016-09-26 20:51:39 -0300

    Create instance of The Godfather

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,6 @@
 import media
 
-toy_story = media.Movie("dummy title", "dummy storyline", "dummy image", "dummy link")
+the_godfather = media.Movie("The Godfather",
+                            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son. ",
+                            "http://dramatica.com/resources/onesheets/the-godfather-one-sheet.jpg",
+                            "https://www.youtube.com/watch?v=sY1S34973zA")
