648967609de2a9ded71e3c082fb4e78728bafd19
Franco Sarachu <fsarachu@gmail.com>
2016-09-26 21:10:28 -0300

    Create Lord of Rings instance

--- entertainment_center.py
+++ entertainment_center.py
@@ -13,5 +13,12 @@ pulp_fiction = media.Movie("Pulp Fiction",
                            "http://cdn.miramax.com/media/assets/Pulp-Fiction1.png",
                            "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
+lord_of_rings = media.Movie("The Lord of the Rings: The Two Towers ",
+                            "While Frodo and Sam edge closer to Mordor with the help of the shifty Gollum, the divided "
+                            "fellowship makes a stand against Sauron's new ally, Saruman, and his hordes of Isengard. ",
+                            "http://www.gstatic.com/tv/thumb/movieposters/30793/p30793_p_v8_am.jpg",
+                            "https://www.youtube.com/watch?v=2dlRvAjU_RI")
+
 print the_godfather.title + ": " + the_godfather.storyline
 print pulp_fiction.title + ": " + pulp_fiction.storyline
+print lord_of_rings.title + ": " + lord_of_rings.storyline
