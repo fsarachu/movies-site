18be08818202440b8639140ff183d0140ac00abe
Franco Sarachu <fsarachu@gmail.com>
2016-09-26 21:04:59 -0300

    Create Pulp Fiction instance

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,4 +6,12 @@ the_godfather = media.Movie("The Godfather",
                             "http://dramatica.com/resources/onesheets/the-godfather-one-sheet.jpg",
                             "https://www.youtube.com/watch?v=sY1S34973zA")
 
-print the_godfather.storyline
+pulp_fiction = media.Movie("Pulp Fiction",
+                           "An insomniac office worker, looking for a way to change his life, crosses paths with a "
+                           "devil-may-care soap maker, forming an underground fight club that evolves into something "
+                           "much, much more. ",
+                           "http://cdn.miramax.com/media/assets/Pulp-Fiction1.png",
+                           "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+
+print the_godfather.title + ": " + the_godfather.storyline
+print pulp_fiction.title + ": " + pulp_fiction.storyline
